74589f4f25461c0035b2c3abc9e28e8fead03bb5
Ilian Shchepinskyi <iliantheone@gmail.com>
2025-09-06 23:28:55 +0300

    Completed the first task

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log('Hello, World!');
+const car =
+{
+	brand: "Toyota",
+	model: "Sprinter Trueno",
+	year: 1985,
+	colors: ["White", "Red", "Black"],
+
+	GetCar: function() { return `Brand: ${this.brand}, Model: ${this.model}, Year: ${this.year}, First color: ${this.colors[0]}`; }
+}
+
+console.log(car.GetCar());
