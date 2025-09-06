3ef6d97df3860f0dc0bf5d52e9f4f22d4d851b38
Ilian Shchepinskyi <iliantheone@gmail.com>
2025-09-06 23:39:41 +0300

    Completed the third task

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log('Hello, World!');
+const student =
+{
+	name: "John Doe",
+	age: 20,
+	grade: 5
+};
+
+const propertyName = "grade";
+const propertyValue = student[propertyName];
+console.log(`${propertyName}: ${propertyValue}`);
